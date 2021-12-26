504f27bb85321edd8252daed2d318236f41744d3
jabber <2898117012@qq.com>
2021-12-26 14:45:05 +0800

    forgot password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8"/>
-    <title>Log In | Hyper - Responsive Bootstrap 5 Admin Dashboard</title>
+    <title>Recover Password | Hyper - Responsive Bootstrap 5 Admin Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description"/>
     <meta content="Coderthemes" name="author"/>
@@ -23,7 +23,6 @@
         <div class="row justify-content-center">
             <div class="col-xxl-4 col-lg-5">
                 <div class="card">
-
                     <!-- Logo -->
                     <div class="card-header pt-4 pb-4 text-center bg-primary">
                         <a href="index.html">
@@ -34,46 +33,33 @@
                     <div class="card-body p-4">
 
                         <div class="text-center w-75 m-auto">
-                            <h4 class="text-dark-50 text-center mt-0 fw-bold">Sign In</h4>
-                            <p class="text-muted mb-4">Enter your email address and password to access admin panel.</p>
+                            <h4 class="text-dark-50 text-center mt-0 fw-bold">Reset Password</h4>
+                            <p class="text-muted mb-4">Enter your email address and we'll send you an email with
+                                instructions to reset your password.</p>
                         </div>
 
                         <form action="#">
-
                             <div class="mb-3">
                                 <label for="emailaddress" class="form-label">Email address</label>
                                 <input class="form-control" type="email" id="emailaddress" required=""
                                        placeholder="Enter your email">
                             </div>
 
-                            <div class="mb-3">
-                                <a href="{{ route('admin.password.request') }}" class="text-muted float-end"><small>Forgot
-                                        your password?</small></a>
-                                <label for="password" class="form-label">Password</label>
-                                <div class="input-group input-group-merge">
-                                    <input type="password" id="password" class="form-control"
-                                           placeholder="Enter your password">
-                                    <div class="input-group-text" data-password="false">
-                                        <span class="password-eye"></span>
-                                    </div>
-                                </div>
+                            <div class="mb-0 text-center">
+                                <button class="btn btn-primary" type="submit">Reset Password</button>
                             </div>
-
-                            <div class="mb-3 mb-3">
-                                <div class="form-check">
-                                    <input type="checkbox" class="form-check-input" id="checkbox-signin" checked>
-                                    <label class="form-check-label" for="checkbox-signin">Remember me</label>
-                                </div>
-                            </div>
-
-                            <div class="mb-3 mb-0 text-center">
-                                <button class="btn btn-primary" type="submit"> Log In</button>
-                            </div>
-
                         </form>
-                    </div> <!-- end card-body -->
+                    </div> <!-- end card-body-->
                 </div>
                 <!-- end card -->
+
+                <div class="row mt-3">
+                    <div class="col-12 text-center">
+                        <p class="text-muted">Back to <a href="{{ route('admin.login') }}" class="text-muted ms-1"><b>Log
+                                    In</b></a></p>
+                    </div> <!-- end col -->
+                </div>
+                <!-- end row -->
 
             </div> <!-- end col -->
         </div>
@@ -84,7 +70,7 @@
 <!-- end page -->
 
 <footer class="footer footer-alt">
-    2018 - 2022 © {{ config('app.name') }}
+    2018 - 2021 © Hyper - Coderthemes.com
 </footer>
 
 <!-- bundle -->
