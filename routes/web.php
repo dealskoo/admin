2c315a823e43c0d9d7b84cc0f879b39a481b69db
jabber <2898117012@qq.com>
2021-12-30 11:40:53 +0800

    fix: login

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,12 @@ use Dealskoo\Admin\Http\Controllers\Auth\NewPasswordController;
 use Dealskoo\Admin\Http\Controllers\Auth\PasswordResetLinkController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix(config('admin.route.prefix'))->name('admin.')->group(function () {
+Route::middleware(['web'])->prefix(config('admin.route.prefix'))->name('admin.')->group(function () {
 
     Route::middleware(['guest'])->group(function () {
+        Route::get('/', function () {
+            return redirect(\route('admin.dashboard'), 301);
+        });
         Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
         Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
         Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
@@ -16,8 +19,8 @@ Route::prefix(config('admin.route.prefix'))->name('admin.')->group(function () {
         Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.update');
     });
 
-    Route::middleware([])->group(function () {
-        Route::get('/', function () {
+    Route::middleware(['auth:admin'])->group(function () {
+        Route::get('/dashboard', function () {
             return view('admin::dashboard');
         })->name('dashboard');
     });
