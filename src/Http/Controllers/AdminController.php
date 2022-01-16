87ede3e68ff086115673c0c636f7d24ee0750446
jabber <2898117012@qq.com>
2022-01-16 22:49:15 +0800

    fix: datatable

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -5,16 +5,59 @@ namespace Dealskoo\Admin\Http\Controllers;
 use Dealskoo\Admin\Models\Admin;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
-use Yajra\DataTables;
+
 class AdminController extends Controller
 {
     public function index(Request $request)
     {
         if ($request->ajax()) {
-
+            return $this->table($request);
         } else {
             return view('admin::admin.index');
         }
+    }
+
+    private function table(Request $request)
+    {
+        $start = $request->input('start', 0);
+        $limit = $request->input('length', 10);
+        $keyword = $request->input('search.value');
+        $columns = ['id', 'name', 'email', 'created_at', 'updated_at', 'status'];
+        $column = $columns[$request->input('order.0.column', 0)];
+        $desc = $request->input('order.0.dir', 'desc');
+        $query = Admin::query();
+        if ($keyword) {
+            $query->where('name', 'like', '%' . $keyword . '%');
+            $query->orWhere('email', 'like', '%' . $keyword . '%');
+        }
+        $query->orderBy($column, $desc);
+        $count = $query->count();
+        $admins = $query->skip($start)->take($limit)->get();
+        $rows = [];
+        foreach ($admins as $admin) {
+            $row = [];
+            $row[] = $admin->id;
+            $row[] = '<img src="' . $admin->avatar_url . '" alt="' . $admin->name . '" title="' . $admin->name . '" class="me-2 rounded-circle"><p class="m-0 d-inline-block align-middle font-16">' . $admin->name . '</p>';
+            $row[] = $admin->email;
+            $row[] = Carbon::parse($admin->created_at)->format('Y-m-d H:i:s');
+            $row[] = Carbon::parse($admin->updated_at)->format('Y-m-d H:i:s');
+            $row[] = $admin->status ? '<span class="badge bg-success">' . __('admin::admin.active') . '</span>' : '<span class="badge bg-danger">' . __('admin::admin.inactive') . '</span>';
+
+            $view_link = '<a href="#" class="action-icon"><i class="mdi mdi-eye"></i></a>';
+
+            $edit_link = '<a href="#" class="action-icon"><i class="mdi mdi-square-edit-outline"></i></a>';
+
+            $destroy_link = '<a href="#" class="action-icon"> <i class="mdi mdi-delete"></i></a>';
+
+            $row[] = $view_link . $edit_link . $destroy_link;
+            $rows[] = $row;
+        }
+        return [
+            'draw' => $request->draw,
+            'recordsTotal' => $count,
+            'recordsFiltered' => $count,
+            'data' => $rows
+        ];
     }
 
     public function create()
--- src/Http/Controllers/RoleController.php
+++ src/Http/Controllers/RoleController.php
@@ -11,10 +11,50 @@ class RoleController extends Controller
     public function index(Request $request)
     {
         if ($request->ajax()) {
-
+            return $this->table($request);
         } else {
             return view('admin::role.index');
         }
+    }
+
+    private function table(Request $request)
+    {
+        $start = $request->input('start', 0);
+        $limit = $request->input('length', 10);
+        $keyword = $request->input('search.value');
+        $columns = ['id', 'name', 'created_at', 'updated_at'];
+        $column = $columns[$request->input('order.0.column', 0)];
+        $desc = $request->input('order.0.dir', 'desc');
+        $query = Role::query();
+        if ($keyword) {
+            $query->where('name', 'like', '%' . $keyword . '%');
+        }
+        $query->orderBy($column, $desc);
+        $count = $query->count();
+        $roles = $query->skip($start)->take($limit)->get();
+        $rows = [];
+        foreach ($roles as $role) {
+            $row = [];
+            $row[] = $role->id;
+            $row[] = $role->name;
+            $row[] = Carbon::parse($role->created_at)->format('Y-m-d H:i:s');
+            $row[] = Carbon::parse($role->updated_at)->format('Y-m-d H:i:s');
+
+            $view_link = '<a href="' . route('admin.roles.show', $role) . '" class="action-icon"><i class="mdi mdi-eye"></i></a>';
+
+            $edit_link = '<a href="' . route('admin.roles.edit', $role) . '" class="action-icon"><i class="mdi mdi-square-edit-outline"></i></a>';
+
+            $destroy_link = '<a href="#" class="action-icon"> <i class="mdi mdi-delete"></i></a>';
+
+            $row[] = $view_link . $edit_link . $destroy_link;
+            $rows[] = $row;
+        }
+        return [
+            'draw' => $request->draw,
+            'recordsTotal' => $count,
+            'recordsFiltered' => $count,
+            'data' => $rows
+        ];
     }
 
     public function create()
