aacda483a955da2dead6519bddcf90c9581cc3fb
jabber <2898117012@qq.com>
2022-01-20 18:23:34 +0800

    test: style

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -13,11 +13,33 @@ class AdminFactory extends Factory
     public function definition()
     {
         return [
+            'avatar' => $this->faker->imageUrl(60, 60),
             'name' => $this->faker->name(),
+            'bio' => $this->faker->text(30),
             'email' => $this->faker->unique->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'owner' => false,
+            'status' => true,
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function isOwner()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'owner' => true,
+            ];
+        });
+    }
+
+    public function inactive()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'status' => false,
+            ];
+        });
     }
 }
