2128cc35bb2d5011404381fcfb9cbf36c893840b
jabber <2898117012@qq.com>
2022-01-23 04:16:43 +0800

    fix: factory

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -16,7 +16,7 @@ class AdminFactory extends Factory
             'avatar' => $this->faker->imageUrl(60, 60),
             'name' => $this->faker->name(),
             'bio' => $this->faker->text(30),
-            'email' => $this->faker->unique->safeEmail(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'owner' => false,
