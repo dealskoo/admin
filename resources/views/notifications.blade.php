277f1815e120ef64e7c004684d29edecc629f3c6
jabber <2898117012@qq.com>
2022-02-17 15:13:40 +0800

    fix: style

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -28,7 +28,7 @@
                                     @foreach($notifications as $notification)
                                         <li @if(!$notification->read_at)class="unread"@endif>
                                             <div class="row">
-                                                <div class="col-lg-10">
+                                                <div class="col-lg-10 ps-4">
                                                     <a href="{{ route('admin.notification.show',$notification) }}">{{ __($notification->data['title']) }}</a>
                                                 </div>
                                                 <div class="col-lg-2">
