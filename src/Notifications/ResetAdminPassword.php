f5994c624b7614bc08045ddc62b12d751c080103
jabber <2898117012@qq.com>
2022-06-09 04:14:23 +0800

    init

--- src/Notifications/ResetAdminPassword.php
+++ src/Notifications/ResetAdminPassword.php
@@ -4,7 +4,6 @@ namespace Dealskoo\Admin\Notifications;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Support\Facades\Lang;
 
 class ResetAdminPassword extends ResetPassword
 {
@@ -16,11 +15,11 @@ class ResetAdminPassword extends ResetPassword
     protected function buildMailMessage($url)
     {
         return (new MailMessage)
-            ->subject(Lang::get('Reset Admin Password Notification'))
-            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::get('Reset Password'), $url)
-            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.admins.expire')]))
-            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
+            ->subject(__('Reset Admin Password Notification'))
+            ->line(__('You are receiving this email because we received a password reset request for your account.'))
+            ->action(__('Reset Password'), $url)
+            ->line(__('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.admins.expire')]))
+            ->line(__('If you did not request a password reset, no further action is required.'));
     }
 
     protected function resetUrl($notifiable)
